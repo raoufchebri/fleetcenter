54e276d2bd556c3ba2fa0aa74fae966b7d818588
Raouf Chebri <raouf@chebri.com>
2020-07-20 15:05:41 +0200

    create car form control

--- src/app/views/modals/create-car/create-car.component.ts
+++ src/app/views/modals/create-car/create-car.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Car } from '../../../core/models';
 import { CarService } from '../../../core/services/car/car.service';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/app.reducers';
 import * as carActions from '../../../core/actions/car.actions';
+
+declare var $: any;
 
 @Component({
   selector: 'app-create-car',
@@ -17,42 +19,45 @@ export class CreateCarComponent implements OnInit {
 
   // Form Controls
   form = new FormGroup({
-    name: new FormControl(),
-    vin: new FormControl(),
-    make: new FormControl(),
-    model: new FormControl(),
-    year: new FormControl(),
-    fuelType: new FormControl(),
+    name: new FormControl('', [Validators.required]),
+    vin: new FormControl('', [Validators.required]),
+    make: new FormControl('', [Validators.required]),
+    model: new FormControl('', [Validators.required]),
+    year: new FormControl('', [Validators.required]),
+    fuelType: new FormControl('', [Validators.required]),
     type: new FormControl(),
     odometer: new FormControl()
   });
   constructor(private carService: CarService, private store: Store<AppState>) { }
 
   ngOnInit(): void {
+
   }
   submit(): void {
-    const { name, vin, make, model, year, fuelType, type, odometer} = this.form.controls;
+    const { name, vin, make, model, year, fuelType, type, odometer } = this.form.value;
     const car: Car = {
-      name: name.value,
-      vin: vin.value,
-      make: make.value,
-      model: model.value,
-      year: year.value,
-      fuelType: fuelType.value,
-      type: type.value,
+      name,
+      vin,
+      make,
+      model,
+      year,
+      fuelType,
+      type,
       position: {
         lat: null,
         lon: null
       },
-      odometer: odometer.value,
+      odometer,
       fuel: null,
       battery: null
     };
-
-    this.store.dispatch(carActions.create({car}));
-    this.form.reset();
+    if (this.form.valid) {
+      this.store.dispatch(carActions.create({ car }));
+      this.form.reset();
+    }
     // const file = this.fileInput.nativeElement.files[0];
     // console.log(file);
-
+    // this.form.reset();
+    $('app-create-car').modal('hide');
   }
 }
